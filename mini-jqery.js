65286f651ede1e051736a12c393fff8b9071e660
Oleh <cherybaoleh@gmail.com>
2019-09-14 11:59:39 +0300

    start

--- mini-jqery.js
+++ mini-jqery.js
@@ -9,34 +9,10 @@ const addHandler = function(el, ev, func) {
   el.addEventListener(ev, func)
 }
 
-const changeHTML = function(el, str) {
-  el.innerHTML = str
-}
-
-const appendHTML = function(el, str) {
-  el.innerHTML += str
-}
-
 Node.prototype.on = function(ev, func) {
   addHandler(this, ev, func)
 }
 
 document.on = function(ev, func) {
   addHandler(this, ev, func)
-}
-
-Node.prototype.html = function(str) {
-  changeHTML(this, str)
-}
-
-document.html = function(str) {
-  changeHTML(this, str)
-}
-
-Node.prototype.append = function(str) {
-  appendHTML(this, str)
-}
-
-document.append = function(str) {
-  appendHTML(this, str)
 }
