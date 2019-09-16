b9dcffe08ee8b5096309aaff49dfb3852c0d958b
Oleh <cherybaoleh@gmail.com>
2019-09-16 18:42:19 +0300

    added encapsulation using IIFE

--- mini-jqery.js
+++ mini-jqery.js
@@ -5,52 +5,54 @@ const $ = str => {
   else return node;
 }
 
-const addHandler = function(el, ev, func) {
-  el.addEventListener(ev, func)
-}
-const removeItem = function(el) {
-  el.parentNode.removeChild(el);
-}
+(function() {
+  const addHandler = function(el, ev, func) {
+    el.addEventListener(ev, func)
+  }
+  const removeItem = function(el) {
+    el.parentNode.removeChild(el);
+  }
 
-const findElem = function(el, str) {
-  let a = $(str);
-  if (a instanceof NodeList) {
-    for(let i = 0; i < a.length; i++) {
-      let node = a[i];
+  const findElem = function(el, str) {
+    let a = $(str);
+    if (a instanceof NodeList) {
+      for(let i = 0; i < a.length; i++) {
+        let node = a[i];
+        while (node.parentNode) {
+          node = node.parentNode;
+          if (node === el) return a[i]
+        }
+      }
+    } else {
+      let node = a;
       while (node.parentNode) {
         node = node.parentNode;
-        if (node === el) return a[i]
+        if (node === el) return a
       }
     }
-  } else {
-    let node = a;
-    while (node.parentNode) {
-      node = node.parentNode;
-      if (node === el) return a
+  }
+
+  Node.prototype.on = function(ev, func) {
+    addHandler(this, ev, func)
+  }
+
+  NodeList.prototype.on = function(ev, func) {
+    for (let i = 0; i < this.length; i++) {
+      addHandler(this[i], ev, func)
     }
   }
-}
 
-Node.prototype.on = function(ev, func) {
-  addHandler(this, ev, func)
-}
-
-NodeList.prototype.on = function(ev, func) {
-  for (let i = 0; i < this.length; i++) {
-    addHandler(this[i], ev, func)
+  Node.prototype.remove = function() {
+    removeItem(this)
   }
-}
 
-Node.prototype.remove = function() {
-  removeItem(this)
-}
-
-NodeList.prototype.remove = function() {
-  for (let i = 0; i < this.length; i++) {
-    removeItem(this[i])
+  NodeList.prototype.remove = function() {
+    for (let i = 0; i < this.length; i++) {
+      removeItem(this[i])
+    }
   }
-}
 
-Node.prototype.find = function(str) {
-  return findElem(this, str)
-}
+  Node.prototype.find = function(str) {
+    return findElem(this, str)
+  }
+})()
